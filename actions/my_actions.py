c3bb9db8c24c10da71608d621b0b5c254ddc08e4
Preston Zen <prestonzen@pm.me>
2021-09-16 01:40:13 -0700

    Repaired Custom Actions + Time Feature + Help
    Debugged Actions File + Added Current Date+Time feature

--- actions/my_actions.py
+++ actions/my_actions.py
@@ -3,20 +3,57 @@
 ##################################################################################
 
 import requests
+import datetime as dt
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
-class MyAction(Action):
+"""
+class ActionMyAction(Action):
 
-    def name(self):
-        return 'action_my_action'
+    def name(self) -> Text:
+        return "action_my_action"
 
-    def run(self, dispatcher, tracker, domain):
+    def run(
+            self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
         # do something.
         return []
+"""
 
+class ActionTellTime(Action):
+
+    def name(self) -> Text:
+        return "action_tell_time"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=f"The current date and time is {dt.datetime.now()}")
+        return []
+
+class ActionSetIPAddress(Action):
+
+    def name(self) -> Text:
+        return "action_set_ip_address"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+        ) -> List[Dict[Text, Any]]:
+        ip_address = tracker.latest_message['ip_address']#Collect IP Address
+        dispatcher.utter_message(text=f"I've saved the IP Address: {ip_address}") #utter to confirm
+        return [SlotSet('ip_address', ip_address)] #First is the Slot, Second is the Python Variable
+
+"""
 class ActionShodanCheck(Action):
 
     def name(self) -> Text:
@@ -29,11 +66,12 @@ class ActionShodanCheck(Action):
         domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
 
-        ip_address = tracker.latest_message['ip address']
+        ip_address = tracker.latest_message['ip_address']
         dispatcher.utter_message(text=f"Here's a Shodan search link: https://www.shodan.io/search?query={ip_address}")
+"""
 
 """
-class ActionIPCheck(Action):
+class ActionIPCheck(Action): #Works Syntactically but not via intent
 
     def name(self) -> Text:
         return "action_ip_check"
@@ -51,8 +89,7 @@ class ActionIPCheck(Action):
         #return [SlotSet("ip_address", user_ip)]
 """
 
-"""
-class ActionReceiveName(Action):
+"""class ActionReceiveName(Action):
 
     def name(self) -> Text:
         return "action_receive_name"
@@ -66,8 +103,7 @@ class ActionReceiveName(Action):
 
         text = tracker.latest_message['text']
         dispatcher.utter_message(text=f"I'll remember your name {text}.")
-        return [SlotSet("name", text)]
-"""
+        return [SlotSet("name", text)]"""
 
 class ActionSetEmail(Action):
 
@@ -83,7 +119,7 @@ class ActionSetEmail(Action):
 
         email = tracker.latest_message['email']
         dispatcher.utter_message(text=f"Confirming the email I have is: {email}")
-        return [SlotSet("email", email)]
+        return [SlotSet("email", email)] #Correct. First section is Slot. Second is Python Variable
 
 class ActionLeakDBCheck(Action):
 
@@ -97,78 +133,57 @@ class ActionLeakDBCheck(Action):
         domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
 
-        email = tracker.get_slot("email")
-        api_call_results = requests.get(
-            f'https://api.weleakinfo.to/api?value={email}&type=email&key=IEMF-RJXT-HEBD-ENDQ')
-        dict_result = api_call_results.json()
-        if dict_result['success']:
-            if dict_result['found'] > 0:
-                all_data = []
-                for each in dict_result['result']:
-                    text = ""
-                    line = each['line'].split(":")
-                    text += "Email: "
-                    text += line[0] + '\n'
-                    text += "Password: "
-                    if len(line) > 1:
-                        text += line[1] + '\n'
-                    else:
-                        continue
-                    text += "Source: "
-                    if len(each['sources']) > 0:
-                        for i in range(len(each['sources'])):
-                            if i == len(each['sources']) - 1:
-                                text += each['sources'][i]
-                            else:
-                                text += each['sources'][i] + ", "
-                        text += '\n'
-                    else:
-                        text += 'Unknown\n'
-                    text += "-" * 20 + '\n'
-                    all_data.append(text)
-                result_length = len(all_data)
-                dispatcher.utter_message(text="I found something.") #utters a message in Zenith
-                for each in all_data[:3]: #remove [:3] to check data before creating a pastebin
-                    dispatcher.utter_message(each.strip())
-                if result_length > 3:
-                    text = ""
-                    for each in all_data:
-                        text += each
-                    data = {'api_dev_key': 'EiL9ngnoApmKM83C0B88aDE23Ud3uSnN',
-                            'api_option': 'paste',  # this will create new paste
-                            'api_paste_private ': '2',
-                            'api_paste_code': text,  # your actual text you want to paste
-                            'api_paste_expire_date': '10M'  # this will make the pastebin link temporary
-                            }
-                    pastebin = requests.post("https://pastebin.com/api/api_post.php", data=data)
-                    dispatcher.utter_message(text=f"You get the picture so rather than me listing all {result_length} of them here's a link with them all" + pastebin.text)
+        email = tracker.get_slot('email')
+        #email = tracker.latest_message["email"]
+        if not email:
+            dispatcher.utter_message(text="I don't have an email to check.")
+        else:
+            api_call_results = requests.get(
+                f'https://api.weleakinfo.to/api?value={email}&type=email&key=1234-1234-1234-1234')
+            dict_result = api_call_results.json()
+            if dict_result['success']:
+                if dict_result['found'] > 0:
+                    all_data = []
+                    for each in dict_result['result']:
+                        text = ""
+                        line = each['line'].split(":")
+                        text += "Email: "
+                        text += line[0] + '\n'
+                        text += "Password: "
+                        if len(line) > 1:
+                            text += line[1] + '\n'
+                        else:
+                            continue
+                        text += "Source: "
+                        if len(each['sources']) > 0:
+                            for i in range(len(each['sources'])):
+                                if i == len(each['sources']) - 1:
+                                    text += each['sources'][i]
+                                else:
+                                    text += each['sources'][i] + ", "
+                            text += '\n'
+                        else:
+                            text += 'Unknown\n'
+                        text += "-" * 20 + '\n'
+                        all_data.append(text)
+                    result_length = len(all_data)
+                    dispatcher.utter_message(text="I found something.") #utters a message in Zenith
+                    for each in all_data[:3]: #remove [:3] to check data before creating a pastebin
+                        dispatcher.utter_message(each.strip())
+                    if result_length > 3:
+                        text = ""
+                        for each in all_data:
+                            text += each
+                        data = {'api_dev_key': 'EiL9ngnoApmKM83C0B88aDE23Ud3uSnN',
+                                'api_option': 'paste',  # this will create new paste
+                                'api_paste_private ': '2',
+                                'api_paste_code': text,  # your actual text you want to paste
+                                'api_paste_expire_date': '10M'  # this will make the pastebin link temporary
+                                }
+                        pastebin = requests.post("https://pastebin.com/api/api_post.php", data=data)
+                        dispatcher.utter_message(text=f"You get the picture so rather than me listing all {result_length} of them here's a link with them all" + pastebin.text)
+                else:
+                    dispatcher.utter_message(text="I didn't find anything useful.")
             else:
                 dispatcher.utter_message(text="I didn't find anything useful.")
-        else:
-            dispatcher.utter_message(text="I didn't find anything useful.")
-
-
-"""
-class LeakDBCheck(Action):
-
-    def name(self):
-        return 'action_leak_db_check'
-
-    def run(self, dispatcher, tracker, domain):
-        email = tracker.latest_message['Email']
-        leak_results_raw = requests.get("https://api.weleakinfo.to/api?value=" + email + "&type=email&key=IEMF-RJXT-HEBD-ENDQ")
-        leak_results = leak_results_raw
-        return leak_results
-"""
-"""
-class ActionLeakDBCheck(Action):
-
-    def name(self) -> Text:
-        return "action_leak_db_check"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="This is what I found")
-"""
+        return []
